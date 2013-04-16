18d644191e7d8bd4941729af29b05264d72b99f4
Arvid E. Picciani <aep@exys.org>
2013-04-16 14:12:49 +0000

    an empty url is valid for LXC

--- src/main/java/hudson/plugins/libvirt/Hypervisor.java
+++ src/main/java/hudson/plugins/libvirt/Hypervisor.java
@@ -74,7 +74,7 @@ public class Hypervisor extends Cloud {
         super("Hypervisor(libvirt)");
         this.hypervisorType = hypervisorType;
         this.hypervisorHost = hypervisorHost;
-        if (hypervisorSystemUrl != null && !hypervisorSystemUrl.equals("")) {
+        if (hypervisorSystemUrl != null) {
             this.hypervisorSystemUrl = hypervisorSystemUrl;
         } else {
             this.hypervisorSystemUrl = "system";
