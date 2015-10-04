700c8a2d6388913f81f1d500cd1f412a65c19938
G.Kr <gkr@gkr.i24.cc>
2015-10-04 17:32:22 +0200

    switch to JenkinsRule - no real test happening

--- src/test/java/hudson/plugins/libvirt/HypervisorTest.java
+++ src/test/java/hudson/plugins/libvirt/HypervisorTest.java
@@ -4,30 +4,27 @@
  */
 package hudson.plugins.libvirt;
 
-import org.jvnet.hudson.test.HudsonTestCase;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.junit.Rule;
+import org.junit.Test;
 
 /**
  *
  * @author mmornati
  */
-public class HypervisorTest extends HudsonTestCase {
+public class HypervisorTest {
+    @Rule public JenkinsRule j = new JenkinsRule();
 
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-
+    @Test
+    public void testDummy() {
+        // placeholder while there are no real tests
     }
-
-    public void testDummy () {
-    	// placeholder while there are no real tests
-    }
-    
-    
     // this doesnt work for everyone ...
-//    public void testCreation() {
-//        Hypervisor hp = new Hypervisor("test", "localhost", 22, "default", "philipp");
-//        assertEquals("Wrong Virtual Machines Size", 4, hp.getVirtualMachines().size());
-//        //assertEquals("Wrong Virtual Machine Name", "test", hp.getVirtualMachines().get(0).getName());
-//        assertEquals("Wrong Hypervisor", hp, hp.getVirtualMachines().get(0).getHypervisor());
-//    }
+    //@Test
+    //public void testCreation() {
+    //    Hypervisor hp = new Hypervisor("test", "localhost", 22, "default", "philipp", 1, false, null);
+    //    assertEquals("Wrong Virtual Machines Size", 4, hp.getVirtualMachines().size());
+    //    //assertEquals("Wrong Virtual Machine Name", "test", hp.getVirtualMachines().get(0).getName());
+    //    assertEquals("Wrong Hypervisor", hp, hp.getVirtualMachines().get(0).getHypervisor());
+    //}
 }
